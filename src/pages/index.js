dd4459b093ea26573a1b47af4c4b9d8be0c49db7
Tony Briget <tony.briget@4d.com>
2022-03-22 18:30:21 +0100

    refactor homepage code

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,6 +47,32 @@ const features = [
   },
 ];
 
+function HeroBanner() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <div className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container text--center padding-vert--lg">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className={clsx(
+              "button button--secondary button--lg",
+              styles.getStarted
+            )}
+            to={useBaseUrl(
+              "docs/getting-started/introduction"
+            )}
+          >
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   const { isDarkTheme } = useColorMode();
@@ -65,7 +91,24 @@ function Feature({ imageUrl, title, description }) {
       </Link>
     </div>
   );
-}
+};
+
+function FeatureContainer() {
+  return (
+    <div className="container text--center margin-vert--lg">
+      <h2>
+        <Translate>Main features</Translate>
+      </h2>
+      <div className={styles.features}>
+        <div className="row margin-top--0">
+          {features.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default function Home() {
   const context = useDocusaurusContext();
@@ -75,38 +118,9 @@ export default function Home() {
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <header className={clsx("hero hero--primary", styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                "button button--outline button--secondary button--lg",
-                styles.getStarted
-              )}
-              to={useBaseUrl(
-                "docs/getting-started/introduction"
-              )}
-            >
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
-
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
+        <HeroBanner />
+        <FeatureContainer />
       </main>
     </Layout>
   );
